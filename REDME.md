e6642ce972e3b06bab638724deb308965af19e18
adress <jaime.aranda271@gmail.com>
2023-02-24 17:08:05 -0500

    first commit

---
+++ REDME.md
@@ -0,0 +1,14 @@
+# GITCHOOSE   
+Herramienta que facilita la modificación de las credenciales de git
+
+## Modo de uso
+1. clone el repostorio
+2. instale las dependencias `npm i`
+3. copie el archivo `accounts.json.example` y cree uno nuevo llamado `accounts.json`  y agrege el nombre y el correo de sus usuarios de git
+```bash
+cp accounts.json.example accounts.json
+```
+4. Ejecute el archivo `app.js` usando node o `ejecute npm start`
+```bash
+node app
+```
---
+++ app.js
@@ -0,0 +1,62 @@
+const inquirer = require('inquirer');
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+const fs = require('fs');
+
+//fields
+let cuentas = {};
+
+
+async function configurarCuenta(cuenta) {
+  await exec(`git config --global user.name "${cuenta.name}"`);
+  await exec(`git config --global user.email "${cuenta.email}"`);
+}
+
+async function printCredenciales() {
+  const { stdout: name } = await exec('git config --global user.name');
+  const { stdout: email } = await exec('git config --global user.email');
+
+  console.log(`usuario: ${name.replace(/(\r\n|\n|\r)/gm, "")}`);
+  console.log(`correo: ${email.replace(/(\r\n|\n|\r)/gm, "")}`);
+}
+
+async function readFile() {
+  return new Promise((resolve, reject) => {
+    fs.readFile(__dirname + '/accounts.json', (err, data) => {
+      if (err) reject(err);
+      resolve(JSON.parse(data));
+    });
+  });
+}
+
+
+async function main() {
+  fileData = await readFile();
+  cuentas = fileData.accounts;
+
+  let choices = cuentas.map((cuenta, index) => index + '. ' + cuenta.name);
+  choices.push(choices.length + '. cancelar');
+
+  console.log('==== gitchoose: Confirgirar credenciales globales de git ====');
+  console.log('Credenciales actuales:');
+  await printCredenciales();
+  inquirer.prompt([
+    {
+      type: 'list',
+      name: 'response',
+      message: 'que cuenta de git quieres usar de manera global?',
+      choices
+    },
+  ]).then(answers => {
+    const index = answers.response.charAt(0);
+    if (index == choices.length - 1) { return; }
+    const cuenta = cuentas[index];
+    configurarCuenta(cuenta).then(() => {
+      console.log('==== Nuevas credenciales globales de git ====');
+      printCredenciales();
+      setTimeout(() => { }, 2000);
+    });
+  });
+}
+
+main();
